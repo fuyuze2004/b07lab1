b3005cb880810c9fc4072f6ee537f93386d4dc0a
fuyuze2004 <yuzefu2004@gmail.com>
2024-09-24 15:10:42 -0400

    modified Polynomial.java and Driver.java

--- Driver.java
+++ Driver.java
@@ -1,13 +1,49 @@
+import java.io.File;
+
 public class Driver {
 	public static void main(String [] args) {
+		System.out.println("Test the blank constructor and evaluate method:");
 		Polynomial p = new Polynomial();
 		System.out.println(p.evaluate(3));
-		double [] c1 = {6,0,0,5};
-		Polynomial p1 = new Polynomial(c1);
-		double [] c2 = {0,-2,0,0,-9};
-		Polynomial p2 = new Polynomial(c2);
+		System.out.println();
+		
+		System.out.println("Test the file constructor:");
+		File f = new File("input.txt");
+		p = new Polynomial(f);
+		for (int i = 0; i < p.coef.length; i++) {
+			System.out.println(p.coef[i] + ", " + p.expo[i]);
+		}
+		System.out.println();
+		
+		System.out.println("Test saveToFile method: check 'ouput.txt'");
+		f = new File("output.txt");
+		p.saveToFile(f);
+		System.out.println();
+		
+		double [] c1 = {6, 5};
+		int [] e1 = {0, 3};
+		Polynomial p1 = new Polynomial(c1, e1);
+		double [] c2 = {-2, -9};
+		int [] e2 = {1, 4};
+		Polynomial p2 = new Polynomial(c2, e2);
+		
+		System.out.println("Test the normal constructor, add method and evaluate method:");
 		Polynomial s = p1.add(p2);
 		System.out.println("s(0.1) = " + s.evaluate(0.1));
+		for (int i = 0; i < s.coef.length; i++) {
+			System.out.println(s.coef[i] + ", " + s.expo[i]);
+		}
+		System.out.println();
+		
+		System.out.println("Test the normal constructor, multiply method and evaluate method:");		
+		Polynomial m = p1.multiply(p2);
+		System.out.println("m(0.1) = " + m.evaluate(0.1));
+		for (int i = 0; i < m.coef.length; i++) {
+			System.out.println(m.coef[i] + ", " + m.expo[i]);
+		}
+		System.out.println();
+		
+		System.out.println("Test the hasRoot method:");
 		if(s.hasRoot(1))
 			System.out.println("1 is a root of s");
 		else
--- Polynomial.java
+++ Polynomial.java
@@ -1,40 +1,163 @@
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Polynomial {
 	double[] coef;
+	int[] expo;
 	
 	public Polynomial() {
-		this.coef = new double[1];
 	}
 	
-	public Polynomial(double arr[]) {
-		int l = arr.length;
+	public Polynomial(double c[], int e[]) {
+		int l = c.length;
 		this.coef = new double[l];
+		this.expo = new int[l];
 		for (int i = 0; i < l; i++) {
-			this.coef[i] = arr[i];
+			this.coef[i] = c[i];
+			this.expo[i] = e[i];
+		}
+	}
+	
+	public Polynomial(File f) {
+		try (Scanner s = new Scanner(f)) {
+			if (s.hasNextLine()) {
+				String line = s.nextLine();
+				String[] parts = line.split("[+-]"), temp;
+				int l = parts.length, j = 0;
+
+				this.coef = new double[l];
+				this.expo = new int[l];
+				for (int i = 0; i < l; i++) {
+					if (parts[i].indexOf('x') == -1) {
+						this.coef[i] = Double.parseDouble(parts[i]);
+					} else {
+						temp = parts[i].split("x");
+						this.coef[i] = Double.parseDouble(temp[0]);
+						this.expo[i] = Integer.parseInt(temp[1]);
+					}
+					if (line.charAt(j) == '-') {
+						this.coef[i] *= -1;
+					}
+					j += parts[i].length();
+				}
+			} 
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void saveToFile(File f) {
+		String line = "";
+		for (int i = 0; i < this.coef.length; i++) {
+			if (this.coef[i] > 0) {
+				line += "+";
+			}
+			if (this.expo[i] == 0) {
+				line += String.valueOf(this.coef[i]).replaceAll("\\.?0*$", "");
+			} else {
+				line += String.valueOf(this.coef[i]).replaceAll("\\.?0*$", "") + "x" + String.valueOf(this.expo[i]);
+			}
+		}
+		line = line.substring(1, line.length());
+		try (BufferedWriter w = new BufferedWriter(new FileWriter(f))) {
+			w.write(line);
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
 	public Polynomial add(Polynomial p) {
 		Polynomial ans;
 		int l_old = this.coef.length, l_add = p.coef.length;
-		if (l_old >= l_add) {
-			ans = new Polynomial(this.coef);
-			for (int i = 0; i < l_add; i++) {
-				ans.coef[i] += p.coef[i];
+		double[] c = new double[l_old + l_add], ans_c;
+		int[] e = new int[l_old + l_add], ans_e;
+		int i = 0, j = 0, k = 0;
+		
+		while (i < l_old && j < l_add) {
+			if (this.expo[i] == p.expo[j]) {
+				c[k] = this.coef[i] + p.coef[j];
+				e[k] = this.expo[i];
+				i++;
+				j++;
+			} else if (this.expo[i] < p.expo[j]) {
+				c[k] = this.coef[i];
+				e[k] = this.expo[i];
+				i++;
+			} else {
+				c[k] = p.coef[j];
+				e[k] = p.expo[j];
+				j++;
 			}
-		} else {
-			ans = new Polynomial(p.coef);
-			for (int i = 0; i < l_old; i++) {
-				ans.coef[i] += this.coef[i];
+			k++;
+		}
+		while (i < l_old) {
+			c[k] = this.coef[i];
+			e[k] = this.expo[i];
+			i++;
+			k++;
+		}
+		while (j < l_add) {
+			c[k] = p.coef[j];
+			e[k] = p.expo[j];
+			j++;
+			k++;
+		}
+		ans_c = new double[k];
+		ans_e = new int[k];
+		for (i = 0; i < k; i++) {
+			ans_c[i] = c[i];
+			ans_e[i] = e[i];
+		}
+		ans = new Polynomial(ans_c, ans_e);
+		
+		return ans;
+	}
+	
+	public Polynomial multiply(Polynomial p) {
+		Polynomial ans;
+		int l_old = this.coef.length, l_mul = p.coef.length;
+		int max_e = this.expo[l_old - 1] + p.expo[l_mul - 1];
+		double[]temp = new double[max_e + 1];
+		double[] ans_c;
+		int[] ans_e;
+		int i , j, k = 0, cnt = 0;
+		
+		for (i = 0; i < l_old; i++) {
+			for (j = 0; j < l_mul; j++) {
+				temp[this.expo[i] + p.expo[j]] += this.coef[i] * p.coef[j];
 			}
 		}
+		for (i = 0; i < max_e + 1; i++) {
+			if (temp[i] != 0) {
+				cnt++;
+			}
+		}
+		ans_c = new double[cnt];
+		ans_e = new int[cnt];
+		for (i = 0; i < max_e + 1; i++) {
+			if (temp[i] != 0) {
+				ans_c[k] = temp[i];
+				ans_e[k] = i;
+				k++;
+			}
+		}
+		ans = new Polynomial(ans_c, ans_e);
 		
 		return ans;
 	}
 	
 	public double evaluate(double x) {
 		double sum = 0;
+		if (this.coef == null) {
+			return sum;
+		}
 		for (int i = 0; i < this.coef.length; i++) {
-			sum += this.coef[i] * Math.pow(x, i);
+			sum += this.coef[i] * Math.pow(x, this.expo[i]);
 		}
 		return sum;
 	}
